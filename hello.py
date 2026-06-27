3b96d046adfa1cbb725281e067a7f0b285800edd
banlang <banlang007zaza@gmail.com>
2026-06-27 11:16:24 +0700

    port

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("hello python")
+from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
+import os
+
+
+class Handler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=os.path.dirname(__file__), **kwargs)
+
+
+if __name__ == "__main__":
+    port = 8000
+    with ThreadingHTTPServer(("0.0.0.0", port), Handler) as httpd:
+        print(f"Serving GPS check-in app at http://localhost:{port}")
+        httpd.serve_forever()
